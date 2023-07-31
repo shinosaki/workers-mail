acc1715bd509597ad877394df9ec6fee00760a9d
Shinosaki <git@shinosaki.com>
2023-08-01 05:33:52 +0900

    [update] frontに<title>タグを追加

---
+++ front/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { PUBLIC_APP_NAME as APP_NAME } from '$env/static/public';
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  <title>{APP_NAME} - {$page.url.hostname}</title>
+</svelte:head>
+
+<slot />
