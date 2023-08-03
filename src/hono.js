aa6a8b8bd828f50ba4125c09bdb46d782f6b3b43
Shinosaki <git@shinosaki.com>
2023-08-04 07:51:17 +0900

    APIにCORSを設定

--- src/hono.js
+++ src/hono.js
@@ -2,16 +2,16 @@ import { Hono } from 'hono';
 const app = new Hono();
 
 // cors
-// import { cors } from 'hono/cors';
-// app.use('*', async (c, next) => {
-//   // https://github.com/honojs/hono/issues/1133#issuecomment-1563573749
-//   const middleware = cors({
-//     origin: () => new URL(c.url).origin,
-//     credentials: true,
-//   });
+import { cors } from 'hono/cors';
+app.use('*', async (c, next) => {
+  // https://github.com/honojs/hono/issues/1133#issuecomment-1563573749
+  const middleware = cors({
+    origin: () => new URL(c.req.url).hostname.replace(/^api\./, ''),
+    credentials: true,
+  });
 
-//   await middleware(c, next);
-// })
+  await middleware(c, next);
+});
 
 import auth from './auth';
 app.route('/api/auth', auth);
