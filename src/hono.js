a6ca5437d1000f3d24167c37c2d694244308ae6d
Shinosaki <git@shinosaki.com>
2023-08-14 12:41:00 +0900

    APIにrobots.txtと404ページを追加した

--- src/hono.js
+++ src/hono.js
@@ -19,4 +19,28 @@ app.route('/api/auth', auth);
 import api from './api';
 app.route('/api/:api_version', api);
 
+app.get('/robots.txt', c => c.text('User-agent: *\n\nDisallow: /'));
+
+const html = `
+<html>
+  <head>
+    <title>Workers Mail API</title>
+  </head>
+
+  <body>
+    <p>It\'s an API for the Workers Mail.</p>
+    <p>More information about the Workers Mail can be found on Github.</p>
+
+    <ul>
+      <li>Github: <a target="_blank" rel="noopener noreferrer" href="https://github.com/shinosaki/workers-mail">https://github.com/shinosaki/workers-mail</a></li>
+    </ul>
+
+    <footer>
+      <p>Workers Mail is developed by <a target="_blank" rel="noopener noreferrer" href="https://shinosaki.com">@shinosaki</p>
+    </footer>
+  </body>
+</html>
+`;
+app.notFound(c => c.html(html, 404));
+
 export default app;
